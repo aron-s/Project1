2f717193b58c6255c0c093b707f5f3404f091a59
aron-s <as4259@njit.edu>
2022-03-27 19:33:33 -0400

    updated pytest

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -9,6 +9,10 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">Glossary of OOP Terms</a>' in response.data
+    assert b'<a class="nav-link" href="/page6">AAA Testing</a>' in response.data
+    assert b'<a class="nav-link" href="/page7">OOPs</a>' in response.data
+    assert b'<a class="nav-link" href="/page8">SOLID Principles</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -46,7 +50,31 @@ def test_request_page4(client):
     assert response.status_code == 200
     assert b"CI/CD-" in response.data
 
-def test_request_page_not_found(client):
+def test_request_page5(client):
     """This makes the index page"""
     response = client.get("/page5")
+    assert response.status_code == 200
+    assert b"Object Oriented Design Patterns Overview-" in response.data
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"AAA Testing-" in response.data
+
+def test_request_page7(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"Object Oriented Programming-" in response.data
+
+def test_request_page8(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"SOLID Principles-" in response.data
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page9")
     assert response.status_code == 404
